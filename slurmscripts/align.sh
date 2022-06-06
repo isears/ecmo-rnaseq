f57eab4127f4246c06748c4e8ad10a99c2a80a7b
Isaac Sears <isaac.j.sears@gmail.com>
2022-06-06 13:43:58 -0400

    Working test alignment

--- slurmscripts/align.sh
+++ slurmscripts/align.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p batch
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=64G
-#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32G
+#SBATCH --time=1:00:00
 #SBATCH --output ./logs/alignment-%j.log
 
 module load python/3.7.4
@@ -11,8 +11,9 @@ module load python/3.7.4
 export PYTHONUNBUFFERED=TRUE
 export PYTHONPATH=./:$PYTHONPATH
 
-cd ~/Repos/ecmo-rnaseq
+cd ~/Repos/ecmo-rnaseq/StarRunDir
 
-# Do something here
+/users/isears1/Repos/STAR/source/STAR --version
+/users/isears1/Repos/STAR/source/STAR --runThreadN 2 --genomeDir /gpfs/data/shared/databases/refchef_refs/grch38_release98/STAR_2_7_10a --readFilesIn ../sampledata/sample.fastq
 
-echo 'slurmscript completed'
+echo 'Alignment completed'
